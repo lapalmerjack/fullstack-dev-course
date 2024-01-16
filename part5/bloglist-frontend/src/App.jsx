e5853550ce04404fc622984929a5d9e3ba0d3137
EJ <ej@EJs-MacBook-Pro.home>
2024-01-16 02:45:20 +0100

    continue learning about reducers

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -13,50 +13,50 @@ const App = () => {
   const blogFormRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
-    //check localstorage for token
-    useEffect(() => {
-      const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
-      if(loggedUserJSON) {
-        const user = JSON.parse(loggedUserJSON)
-        setUser(user)
-        blogService.setToken(user.token)
-      }
-    }, [])
-  
-    const logout = () => {
-      window.localStorage.removeItem('loggedBlogappUser')
-      setUser(null)
+  //check localstorage for token
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
     }
+  }, [])
 
-    const createBlog = async (blogObject) => {
-     try {
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  const createBlog = async (blogObject) => {
+    try {
       const returnedBlog = await blogService.createBlog(blogObject)
       setBlogs(blogs.concat(returnedBlog))
-      }  catch(error) {
-        console.error('Error creating a new blog:', error);
-      }   
+    }  catch(error) {
+      console.error('Error creating a new blog:', error)
     }
+  }
 
-    const updateLikes = async (id, blogObject) => {
-      try{
-        const updatedBlog = await blogService.updateLikes(id, blogObject)
-        console.log('updated blog: ', updatedBlog)
-        setBlogs(blogs.map(blog => blog.id !== id ? blog : updatedBlog))
-      }catch(error) {
-        console.error('Error updating blog post ', error)
-      }   
+  const updateLikes = async (id, blogObject) => {
+    try{
+      const updatedBlog = await blogService.updateLikes(id, blogObject)
+      console.log('updated blog: ', updatedBlog)
+      setBlogs(blogs.map(blog => blog.id !== id ? blog : updatedBlog))
+    }catch(error) {
+      console.error('Error updating blog post ', error)
     }
-    const deleteBlog = async (id) => {
-      const blogDeleter = await blogService.deleteBlog(id)
-      console.log('blog deleted')
-      setBlogs(blogs.filter(blog => blog.id !== id))
-    }
-    const sortByLikes = [...blogs].sort((a, b) => b.likes - a.likes);
-  
-  
+  }
+  const deleteBlog = async (id) => {
+    const blogDeleter = await blogService.deleteBlog(id)
+    console.log('blog deleted')
+    setBlogs(blogs.filter(blog => blog.id !== id))
+  }
+  const sortByLikes = [...blogs].sort((a, b) => b.likes - a.likes)
+
+
 
   if (user === null) {
     return (
@@ -73,14 +73,14 @@ const App = () => {
       <h2>blogs</h2>
       <Notification message={message} />
       <p>{user.name} logged in
-      <button onClick={() => logout()}>logout</button> </p>
+        <button onClick={() => logout()}>logout</button> </p>
       <Togglable buttonLabel="new blog" ref={blogFormRef}>
-      <BlogForm setMessage={setMessage} createBlog={createBlog} user={user} />
+        <BlogForm setMessage={setMessage} createBlog={createBlog} user={user} />
       </Togglable>
       {sortByLikes.map(
         blog =>
-        <Blog user={user} key={blog.id} blog={blog} updateLikes={updateLikes}
-         deleteBlog={deleteBlog} />
+          <Blog user={user} key={blog.id} blog={blog} updateLikes={updateLikes}
+            deleteBlog={deleteBlog} />
       )}
     </div>
   )
